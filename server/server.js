c40a60284252ddc87f86e248b001f5d9aa4f1cd9
Dillon Smith <dillsmth@gmail.com>
2021-09-21 18:10:41 -0500

    changing connection usefindandmodify to true

--- server/server.js
+++ server/server.js
@@ -6,36 +6,71 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// const app = express();
+// const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware
+// });
 
-//integrate apollo server with express app
-server.applyMiddleware({ app });
+// //integrate apollo server with express app
+// server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   // set static folder
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-// comment this out at end of mern development
+// // comment this out at end of mern development
 
-app.get('*',  (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
+// app.get('*',  (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// })
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`🌍 Now listening on localhost:${PORT}`);
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
+
+async function startApolloServer() {
+  const app = express();
+  const PORT = process.env.PORT || 3001;
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+    ,
   });
-});
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // Additional middleware can be mounted at this point to run before Apollo.
+  if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+
+  // Mount Apollo middleware here.
+  app.get('*',  (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+}
