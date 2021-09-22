d15d520d6c8bc8556a73d940519d0b42485eb431
Dillon Smith <dillsmth@gmail.com>
2021-09-22 08:39:39 -0500

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const server = new ApolloServer({
 server.applyMiddleware({ app });
 
 // Express middleware for parsing
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
