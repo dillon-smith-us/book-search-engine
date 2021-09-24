b4f9a682476d6eae9bfc1ead720de47d63706d07
Dillon Smith <dillsmth@gmail.com>
2021-09-23 20:05:11 -0500

    fixing typedef mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,46 +1,49 @@
-// import gql tagged template function
+// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-// create typeDefs
+// create our typeDefs
 const typeDefs = gql`
-    type Book {
-        _id: ID
-        authors: [String]
-        description: String
-        bookId: String
-        image: String
-        link: String
-        title: String
-    }
-    type User {
-        _id: ID
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-    }
-    type Auth {
-        token: ID!
-        user: User
-    }
-    type Query {
-        me: User
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(book: SavedBookInput): User
-        removeBook(bookId: String!): User
-    }
-    input SavedBookInput {
-        authors: [String]
-        description: String
-        bookId: String
-        image: String
-        link: String
-        title: String
-    }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    title: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  input bookDetails {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookDetails): User
+    removeBook(bookId: String!): User
+  }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
